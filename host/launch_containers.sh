8761c4ccabdb89748e3d24c4d016eb5cdd1812f2
Jamie Rajewski <jamie.rajewski1@gmail.com>
2021-06-24 16:55:26 -0600

    Remove unnecessary files; add method of launching containers

--- host/launch_containers.sh
+++ host/launch_containers.sh
@@ -2,6 +2,10 @@
 
 # Launch the corresponding containers depending on which variables are set
 
+## TODO
+# - Add safety check to ensure we cant launch more than one instance of each container
+#   - Do something like in launch_processing.sh - if [ podman ps | grep container_name ] etc...
+
 #################
 OFFLINE=false
 BENCH=false
@@ -10,17 +14,17 @@ PRODUCTION=false
 #################
 
 if [ "$OFFLINE" = true ]; then
-
+    singularity instance start $HOME/containers/processing_container.sif offline_processing 
 fi
 
 if [ "$BENCH" = true ]; then
-
+    singularity instance start $HOME/containers/processing_container.sif benchmarking
 fi
 
 if [ "$PROCESSING" = true ]; then
-
+    singularity instance start $HOME/containers/processing_container.sif enqueue_processing
 fi
 
 if ["$PRODUCTION" = true ]; then
-
+    singularity instance start $HOME/containers/processing_container.sif enqueue_production
 fi
--- processing/processing_env.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source /home/snoprod/data-flow/grid/cern_env.sh
-source /home/snoprod/dirac_ui/bashrc
-cd /home/snoprod/data-flow && source env.sh
